317688c1a15bf274a083588fdfa1933740e2f1fa
Hossein <Hossein.kelisa@gmail.com>
2025-08-03 22:31:48 +0200

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,49 @@
-import React from 'react';
-import './Navbar.css'; 
+import React, { useState } from "react";
+import "./Navbar.css";
+import { FiMenu, FiX } from "react-icons/fi";
+import logo from "/logo.png"; 
 
-const Navbar = () => (
-  <nav className="navbar">
-    <div className="logo">Hossein.dev</div>
-    <ul className="nav-links">
-      <li><a href="#about">About</a></li>
-      <li><a href="#projects">Projects</a></li>
-        <li><a href="#certificates">Certificates</a></li>
-      <li><a href="#resume">Resume</a></li>
-      <li><a href="#contact">Contact</a></li>
-    </ul>
-  </nav>
-);
+const Navbar = () => {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const closeMobileMenu = () => setMobileMenuOpen(false);
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-left">
+        <img src={logo} alt="Logo" className="logo" />
+      </div>
+
+      <button className="mobile-menu-icon" onClick={toggleMobileMenu}>
+        {isMobileMenuOpen ? <FiX size={24} /> : <FiMenu size={24} />}
+      </button>
+
+      <ul
+        className={`nav-links ${isMobileMenuOpen ? "open" : ""}`}
+        onClick={closeMobileMenu}
+      >
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#projects">Projects</a>
+        </li>
+        <li>
+          <a href="#certificates">Certificates</a>
+        </li>
+        <li>
+          <a href="#resume">Resume</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;
