c42fd90eb4109e76bef50a7dd3a2ec2a295c8c40
Hossein <Hossein.kelisa@gmail.com>
2025-08-04 10:47:17 +0200

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import About from './components/About';
-import Projects from './components/Projects';
-import Resume from './components/Resume';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import Certificates from './components/Certificates';
-import './index.css';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Resume from "./components/Resume";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Certificates from "./components/Certificates";
+import "./index.css";
 
 const App = () => {
   return (
