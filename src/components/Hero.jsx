9b62ec737f5fb18dfe329c89f59013afcf7bc710
Hossein <Hossein.kelisa@gmail.com>
2025-08-08 14:31:31 +0200

    update it

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import "./Hero.css";
+import Fades from "../Animations/Fades"; 
 
 const Hero = () => (
   <section className="hero-section">
+    <Fades animationType="fadeZoom">
     <h1 className="hero-title">
       Hi there, I'm <span>Hossein</span>
     </h1>
+    </Fades>
     <p className="hero-subtitle">
       A passionate web developer building responsive and user-friendly websites.
     </p>
+    <Fades animationType="fadeZoom">
     <a href="#projects" className="hero-button">
       View My Work
     </a>
+    </Fades>
   </section>
 );
 
