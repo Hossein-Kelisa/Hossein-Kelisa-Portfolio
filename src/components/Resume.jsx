1dc37e30934d8376d0da2aa23a577726a3374e74
Hossein <Hossein.kelisa@gmail.com>
2025-08-08 22:32:57 +0200

    update it

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import './Resume.css'; 
+import React from "react";
+import Fades from "../Animations/Fades";
+import "./Resume.css";
 
 const Resume = () => (
   <section id="resume" className="resume-section">
-    <h2>Resume</h2>
-    <p>.........</p>
+    <Fades animationType="fadeUp">
+      <h2>Resume</h2>
+    </Fades>
+    <Fades animationType="fadeUp">
+      <p>You can download my resume here:</p>
+      <a
+        href="/HosseinKelisa.pdf"
+        download
+        className="download-btn"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Download
+      </a>
+    </Fades>
   </section>
 );
 
