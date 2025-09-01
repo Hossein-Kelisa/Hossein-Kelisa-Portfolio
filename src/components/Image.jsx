32aa254ac6e39c21ef88fb6c2c53b6b8d528c923
Hossein <Hossein.kelisa@gmail.com>
2025-09-01 11:57:49 +0200

    wavy background

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
 import Fades from "../Animations/Fades";
 import IMG from "../assets/my-image.jpg"; 
 import './Image.css';
+import { Waves } from "./Waves";
 
 const Image = () => {
     return (
         <div className="image-container">
+        <Waves />
         <Fades animationType="fadeRotate">
             <img className="my-image" src={IMG} alt="my image" />
         </Fades>
