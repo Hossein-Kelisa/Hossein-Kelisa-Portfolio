4acff0720ac592399ed06800cecd92a34ebd8da5
Hossein <Hossein.kelisa@gmail.com>
2025-09-01 16:33:48 +0200

    image zoom

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
 import Fades from "../Animations/Fades";
-import IMG from "../assets/my-image.jpg"; 
-import './Image.css';
+// import IMG from "../assets/my-image.jpg";
+import "./Image.css";
 
 const Image = () => {
-    return (
-        <div className="image-container">
-        <Fades animationType="fadeRotate">
-            <img className="my-image" src={IMG} alt="my image" />
-        </Fades>
-        </div>
-    );
+  return (
+    <div className="image-container">
+      <Fades animationType="fadeRotate">
+        {/* <img className="my-image" src={IMG} alt="my image" /> */}
+        <div className="my-image" />
+      </Fades>
+    </div>
+  );
 };
 
 export default Image;
