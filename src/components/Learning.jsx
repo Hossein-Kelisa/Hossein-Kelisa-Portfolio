9b3038a7fa1af48f187b930fa9feaa09f711ec14
Hossein <Hossein.kelisa@gmail.com>
2025-09-21 21:19:49 +0200

    new learning

---
+++ src/components/Learning.jsx
@@ -0,0 +1,17 @@
+import "./LearningNow.css";
+
+function LearningNow() {
+  return (
+    <div className="learning-box">
+      <h2>📚 Learning Now</h2>
+      <ul>
+        <li>TypeScript</li>
+        <li>CSS (Tailwind coming later 😉)</li>
+        <li>English</li>
+        <li>Dutch</li>
+      </ul>
+    </div>
+  );
+}
+
+export default LearningNow;
