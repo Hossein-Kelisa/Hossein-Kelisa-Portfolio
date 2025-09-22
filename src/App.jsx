1f30a516b14c3f48b03efd75d196dac006b2a7fe
Hossein <Hossein.kelisa@gmail.com>
2025-09-22 22:04:33 +0200

    new learning

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Layout from "./components/Layout";
 import Skills from "./components/Skills";
+import Learning from "./components/Learning";
 import "./i18n";
 import "./index.css";
 
@@ -20,6 +21,7 @@ const App = () => {
         <Hero />
         <Layout />
         <Skills />
+        <Learning />
         <Resume />
         <Contact />
       </main>
