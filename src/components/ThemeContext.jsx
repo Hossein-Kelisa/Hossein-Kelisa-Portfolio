3a6ecce57e66ae355cfde0439902a079f4e42e67
Hossein <Hossein.kelisa@gmail.com>
2025-09-23 21:54:26 +0200

    dark mode

---
+++ src/components/ThemeContext.jsx
@@ -0,0 +1,29 @@
+import { createContext, useState, useEffect } from "react";
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const ThemeContext = createContext();
+
+export function ThemeProvider({ children }) {
+  const [darkMode, setDarkMode] = useState(() => {
+    // check saved theme in localStorage
+    return localStorage.getItem("theme") === "dark";
+  });
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.body.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [darkMode]);
+
+  const toggleTheme = () => setDarkMode(!darkMode);
+
+  return (
+    <ThemeContext.Provider value={{ darkMode, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
