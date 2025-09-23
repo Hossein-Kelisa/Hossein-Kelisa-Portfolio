6049f220a55ca9b4c23e1dbede89b715973eadff
Hossein <Hossein.kelisa@gmail.com>
2025-09-23 22:13:24 +0200

    dark mode

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import App from "./App.jsx";
 import "./i18n.js";
 import ReactDOM from "react-dom/client";
-import { ThemeProvider } from "./ThemeContext";
+import { ThemeProvider } from "./components/ThemeContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ThemeProvider>
