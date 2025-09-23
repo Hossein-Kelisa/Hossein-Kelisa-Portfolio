1074174cd75d2a1ee3826e983fa4aeffe4c28a49
Hossein <Hossein.kelisa@gmail.com>
2025-09-23 22:24:02 +0200

    dark mode

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { ThemeContext } from "./ThemeContext";
+import "./ThemeToggle.css";
 
 function ThemeToggle() {
   const { darkMode, toggleTheme } = useContext(ThemeContext);
