eaba5ffcc1e2d1b318cfcbd91131e5336e97b492
Hossein <Hossein.kelisa@gmail.com>
2025-11-14 17:25:56 +0100

    install package Router and initrilze Services page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
+import Services from "./Pages/ServicesPage";
 import "./index.css";
 
 const App = () => {
   return (
-    <>
-      <HomePage />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/services" element={<Services />} />
+      </Routes>
+    </Router>
   );
 };
 export default App;
