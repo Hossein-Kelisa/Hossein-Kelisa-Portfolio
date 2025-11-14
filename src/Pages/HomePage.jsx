e1fdf55f218a5231e2de67425be7e97719f4b056
Hossein <Hossein.kelisa@gmail.com>
2025-11-14 17:12:29 +0100

    separate pages

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,38 @@
+import ToggleLanguages from "../components/ToggleLanguages";
+import ThemeToggle from "../components/ThemeToggle";
+import Navbar from "../components/Navbar";
+import Image from "../components/Image";
+import Hero from "../components/Hero";
+import Resume from "../components/Resume";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
+import Layout from "../components/Layout";
+import Skills from "../components/Skills";
+import Learning from "../components/Learning";
+import Proposal from "../components/Proposal";
+import Hobbies from "../components/Hobbies";
+import "../i18n";
+
+const App = () => {
+  return (
+      <>
+        <ToggleLanguages />
+        <ThemeToggle />
+        <Navbar />
+        <main>
+          <Image />
+          <Hero />
+          <Layout />
+          <Skills />
+          <Learning />
+          <Hobbies />
+          <Proposal />
+          <Resume />
+          <Contact />
+        </main>
+        <Footer />
+      </>
+  );
+};
+
+export default App;
