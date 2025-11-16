d520253a29c900fc314faeeb5f200c31b71fd94a
Hossein <Hossein.kelisa@gmail.com>
2025-11-16 20:24:39 +0100

    commit some component for now

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -8,11 +8,11 @@ import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 import Layout from "../components/Layout";
 import Skills from "../components/Skills";
-import Learning from "../components/Learning";
-import MyLanguages from "../components/MyLanguages";
-import RequirementForm from "../components/RequirementForm";
-import Proposal from "../components/Proposal";
-import Hobbies from "../components/Hobbies";
+// import Learning from "../components/Learning";
+// import MyLanguages from "../components/MyLanguages";
+// import RequirementForm from "../components/RequirementForm";
+// import Proposal from "../components/Proposal";
+// import Hobbies from "../components/Hobbies";
 import ScrollTop from "../components/ScrollTop";
 import "../i18n";
 
@@ -28,11 +28,11 @@ const HomePage = () => {
         <ScrollTop />
         <Layout />
         <Skills />
-        <Learning />
-        <MyLanguages />
-        <RequirementForm />
-        <Hobbies />
-        <Proposal />
+        {/* <Learning /> */}
+        {/* <MyLanguages /> */}
+        {/* <RequirementForm /> */}
+        {/* <Hobbies /> */}
+        {/* <Proposal /> */}
         <Resume />
         <Contact />
       </main>
