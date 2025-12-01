6b4eca83f8a166c7e3b87a1bbb22893fe8f74ede
Hossein <Hossein.kelisa@gmail.com>
2025-12-01 19:53:38 +0100

    add upload file in routes

--- portfolio/server/routes/contact.js
+++ portfolio/server/routes/contact.js
@@ -1,9 +1,10 @@
-import express from 'express';
-import { sendMessage } from '../controller/contactController.js';
-import { validateContact } from '../middleware/validateContact.js';
+import express from "express";
+import { sendMessage } from "../controller/contactController.js";
+import { validateContact } from "../middleware/validateContact.js";
+import upload from "../middleware/uploadFile.js";
 
 const router = express.Router();
 
-router.post('/', validateContact, sendMessage);
+router.post("/", upload.single("attachment"), validateContact, sendMessage);
 
 export default router;
