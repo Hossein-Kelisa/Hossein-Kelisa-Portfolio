c75ffc82248ba3dbdb7caea35cc4c19bf3d1e33c
Hossein <Hossein.kelisa@gmail.com>
2025-12-01 21:13:47 +0100

    change importing format

--- portfolio/server/routes/contact.js
+++ portfolio/server/routes/contact.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { sendMessage } from "../controller/contactController.js";
 import { validateContact } from "../middleware/validateContact.js";
-import upload from "../middleware/uploadFile.js";
+import { upload } from "../middleware/uploadFile.js";
 
 const router = express.Router();
 
