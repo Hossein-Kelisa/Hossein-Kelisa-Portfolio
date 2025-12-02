1e64971aa8a327d10184f6312809d9cf9e88a3dc
Hossein <Hossein.kelisa@gmail.com>
2025-12-02 22:45:18 +0100

    send email

---
+++ portfolio/server/utils/sendEmail.js
@@ -0,0 +1,26 @@
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export const sendEmail = async ({ name, email, message, attachments}) => {
+  const response = await resend.emails.send({
+    from: "Portfolio <onboarding@resend.dev>",
+    to: process.env.MY_EMAIL,
+    subject: "Portfolio form submission",
+    text: `From: ${name}\nEmail: ${email}\n\n${message}`,
+    html: `
+      <div>
+        <strong>Name:</strong> ${name}<br/>
+        <strong>Email:</strong> ${email}<br/>
+        <p style="white-space: pre-wrap;">${message}</p>
+      </div>
+    `,
+    attachments: attachments || [],
+  });
+
+  if (response.error) {
+    console.log("❌ Email failed:", response.error);
+  } else {
+    console.log("✅ Email sent:", response.id);
+  }
+};
